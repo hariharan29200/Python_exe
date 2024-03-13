53421d244538ecc63cb476f480c2900e5835abe3
hariharan29200 <kumarselvy19@gmail.com>
2024-03-13 14:08:48 +0530

    Python exe

---
+++ exe1.py
@@ -0,0 +1,10 @@
+print("hello")
+
+x=1
+#comments 
+if(x>2):
+    print("5 is greater than 2")
+else:
+    print('2 is greater than 5')
+    
+    
---
+++ loop.py
@@ -0,0 +1,44 @@
+i=0
+while i<10:
+    print(i)
+    i=i+1
+print()
+
+j=0
+while j<10:
+    j=j+1
+    if j==5:
+        continue
+    print(j)
+print()
+
+k=0
+while k<10:
+    if k==7:
+        break
+    print(k)
+    k=k+1
+print()
+
+for x in "vavuniya":
+    print(x)
+print()
+
+print(x)
+
+for x in range(5): #(it starts from 0 to 4)
+    print(x)
+print()
+
+
+for x in range(2,6): #(start range, end range)
+    print(x)
+print()
+
+
+for x in range(2,10,2): #(start range, end range, increment)
+    print(x)
+print()
+
+
+    
